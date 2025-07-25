fd882647d8bd5f661bd1d1da589242a9eabe82f0
Damien <dtriegaardt@outlook.com>
2025-07-25 21:49:50 +1000

    Refactor components

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,3 +1,16 @@
-export default function TodoCard() {
-  return <div>TodoCard</div>;
+import PropTypes from 'prop-types';
+
+export default function TodoCard(props) {
+  const { children } = props;
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <i className="fa-solid fa-pen-to-square" />
+        <i className="fa-regular fa-trash-can" />
+      </div>
+    </li>
+  );
 }
+
+TodoCard.propTypes = { children: PropTypes.string.isRequired };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,5 @@
+import TodoCard from './TodoCard';
+
 export default function TodoList() {
   const todoList = ['Task 1', 'Task 2', 'Task 3'];
 
@@ -5,10 +7,9 @@ export default function TodoList() {
     <ul className="main">
       {todoList.map((todo) => {
         return (
-          <li className="todoItem" key={todo.id}>
-            {todo}
-            <i className="fa-solid fa-pen-to-square" />
-          </li>
+          <TodoCard key={todo.id}>
+            <p>{todo}</p>
+          </TodoCard>
         );
       })}
     </ul>
