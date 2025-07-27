9d5d45c0836d803e95982def8ed6a80866a57043
Damien <dtriegaardt@outlook.com>
2025-07-27 19:50:49 +1000

    Refactor function that updates todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
 function App() {
   const [todos, setTodos] = useState(['Task 1', 'Task 2', 'Task 3']);
 
-  function handleAddTodos(newTodo) {
-    const newTodoList = [...todos, newTodo];
-    setTodos(newTodoList);
-  }
+  const handleAddTodos = useCallback((newTodo) => {
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  }, []);
 
   return (
     <>
